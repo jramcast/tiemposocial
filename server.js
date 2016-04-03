770b98551a6eb8cd6227853d4a75469b2cb98931
Jaime <jaime.ram@gmail.com>
2016-04-03 19:59:57 +0200

    Extracted twitter tokenExtracted twitter tokenss

--- server.js
+++ server.js
@@ -10,31 +10,24 @@ var ClientFactory = require('./lib/clientfactory');
 // Configuration
 require('./config/config.js')(app, express);
 
-
-
 ////////// SINGLETONS ///////////////////////////////////////////
-/* Initialize Twitter API */
-/*var config = {	
-	action: "filter", 
-	track: Common.twitterStreamKeywords,
-	user: "#######",
-	password: "########"
-};*/
 
 var config = {
-  consumer_key: 'MNOYxcNXehdG2KbU5epzGoDTf',
-  consumer_secret: 't4j6qJ4Pd7aMfs2afN32AWVgatCH6kdvxHh3m9itAud4ziHLML',
-  access_token_key: '52827928-qR2y5T4F2MeExz6Pc0wbI4H4TqTHDVHoSUBUU5FyV',
-  access_token_secret: 'raiXTSFuoSIgc8OwTrxBe8RkPlftIarFfpuf4oWWQBNgV'
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 };
+
+/* Initialize Twitter API */
 var twitterAPI  = new Twitter(config);
 
-/*Initialize Client Factory*/
+/* Initialize Client Factory */
 var clientFactory = new ClientFactory(twitterAPI);
 module.exports.clientFactory = clientFactory;
 
 
-/*Initialize Socket.IO*/
+/* Initialize Socket.IO */
 var socketManager = SocketsManager.createWithAppReference(app);
 /////////////////////////////////////////////////////
 
@@ -43,10 +36,10 @@ var socketManager = SocketsManager.createWithAppReference(app);
 /* Set Routes */
 Router.createRoutes(app);
 
-/*catch any exception*/
-/*process.on('uncaughtException', function (err) {
+/* catch any exception */
+process.on('uncaughtException', function (err) {
   console.log('Caught exception: ' + err);
-});*/
+});
 
 //Start Server App
 if(!module.parent) {
